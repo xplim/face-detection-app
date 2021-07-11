99301e50c7916b1f13083d9db127f232c12f1fb3
xplim <75422036+xplim@users.noreply.github.com>
2021-07-11 16:56:13 +0800

    Add particles styling to background

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Particles from 'react-particles-js';
 import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
@@ -5,8 +6,25 @@ import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
 
 function App() {
+  const particlesOptions = {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      line_linked: {
+        opacity: 0.7,
+        width: 1,
+      },
+    },
+  };
+
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
