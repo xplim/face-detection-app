5f00774010df1817fd9c5e3be9ef0a3255f9ed6d
xplim <75422036+xplim@users.noreply.github.com>
2021-07-11 16:28:43 +0800

    Add Rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="dark-pink f3">Your current rank is...</div>
+      <div className="dark-pink f1">#5</div>
+    </div>
+  );
+};
+
+export default Rank;
