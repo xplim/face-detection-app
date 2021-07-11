342bb00c5ae4d8f2134bed8657b1bb7bac74304f
xplim <75422036+xplim@users.noreply.github.com>
2021-07-11 16:15:39 +0800

    Add ImageLinkForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <ImageLinkForm />
     </div>
   );
 }
