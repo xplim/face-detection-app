404f1075d2f04d03d3c8483c75927afee1363a80
xplim <75422036+xplim@users.noreply.github.com>
2021-07-12 18:03:07 +0800

    Connect sign in route to API

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { RouteContext } from '../../contexts/RouteContext';
 
 // Sources:
@@ -6,12 +6,39 @@ import { RouteContext } from '../../contexts/RouteContext';
 //   http://tachyons.io/components/cards/product-card/index.html
 
 const SignIn = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const { routes, setRoute } = useContext(RouteContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('http://localhost:4000/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        throw new Error(`${response.status}: ${response.statusText}`);
+      })
+      .then((result) => {
+        setRoute(routes.HOME);
+      })
+      .catch((err) => console.error(err.message));
+  };
 
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure">
+        <form className="measure" onSubmit={(event) => onSubmit(event)}>
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
@@ -24,6 +51,7 @@ const SignIn = () => {
                 name="email-address"
                 id="email-address"
                 autoComplete="email"
+                ref={emailRef}
               />
             </div>
             <div className="mv3">
@@ -36,6 +64,7 @@ const SignIn = () => {
                 name="password"
                 id="password"
                 autoComplete="current-password"
+                ref={passwordRef}
               />
             </div>
           </fieldset>
@@ -44,7 +73,6 @@ const SignIn = () => {
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
               type="submit"
               value="Submit"
-              onClick={() => setRoute(routes.HOME)}
             />
           </div>
           <div className="lh-copy mt3">
